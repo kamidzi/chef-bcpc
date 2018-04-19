818daf5a5d75cff3e8180d231ffb3e07e364dd23
Kundai Andrew Midzi <kmidzi@bloomberg.net>
2018-04-19 15:36:36 -0400

    Memoize vagrant ssh-config for bootstrap vm.

--- utils/dist-src.sh
+++ utils/dist-src.sh
@@ -1,16 +1,20 @@
 #/bin/bash
-
 hash -r
 set -e
 
 dir="$(git rev-parse --show-toplevel)" || exit 2
-keyfile="$(
+VM_BOOTSTRAP_CONFIG="$(
   cd "${dir}/bootstrap/vagrant_scripts"
-  vagrant ssh-config bootstrap | awk '/Host bootstrap/,/^$/{ if ($0 ~ /^ +IdentityFile/) print $2}'
+  vagrant ssh-config bootstrap
 )"
-port="$(
-  cd "${dir}/bootstrap/vagrant_scripts"
-  vagrant ssh-config bootstrap | awk '/Host bootstrap/,/^$/{ if ($0 ~ /^ +Port/) print $2}'
+
+keyfile="$(awk '
+    /Host bootstrap/,/^$/{ if ($0 ~ /^ +IdentityFile/) print $2}
+  ' <<< "$VM_BOOTSTRAP_CONFIG"
+)"
+port="$(awk '
+    /Host bootstrap/,/^$/{ if ($0 ~ /^ +Port/) print $2}
+  ' <<< "$VM_BOOTSTRAP_CONFIG"
 )"
 
 rsync -axSHvP -e "ssh -oPort=$port -ostricthostkeychecking=no -i ${keyfile}" --exclude vbox --exclude vmware --exclude vbox/insecure_private_key --exclude .chef . vagrant@127.0.0.1:chef-bcpc
